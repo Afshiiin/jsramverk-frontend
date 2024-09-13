75b52d96b7fda681166f7bc1aaa226671f35de87
Afshin <afshin11se@hotmail.com>
2024-09-13 14:00:39 +0200

    fixed tests path

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render} from '@testing-library/react';
 
-import Editor from '../App.js'; // Adjust the import path as needed
+import Editor from '.././app.js'; // Adjust the import path as needed
 
 
 test('Get title', () => {
